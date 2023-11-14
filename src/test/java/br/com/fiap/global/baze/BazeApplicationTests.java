7a21645557cdcb40ccbb73eae2f12715195cad6c
matlencar <rm94642@fiap.com.br>
2023-11-14 06:28:26 -0300

    first commit to baze

---
+++ src/test/java/br/com/fiap/global/baze/BazeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.global.baze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
