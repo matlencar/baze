4475701ad6889729157a53b52f2e3a67fe5673b9
matlencar <rm94642@fiap.com.br>
2023-11-15 10:55:13 -0300

    chore: incluindo anotação @Data nas models

--- src/main/java/br/com/fiap/global/baze/model/Bicicleta.java
+++ src/main/java/br/com/fiap/global/baze/model/Bicicleta.java
@@ -5,12 +5,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
--- src/main/java/br/com/fiap/global/baze/model/Telefone.java
+++ src/main/java/br/com/fiap/global/baze/model/Telefone.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +14,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 @Entity
 public class Telefone {
     
