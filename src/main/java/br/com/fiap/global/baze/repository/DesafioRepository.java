a66d2eb7adfafbdd1ffc42d8a188f9df8201d26e
matlencar <rm94642@fiap.com.br>
2023-11-16 19:12:02 -0300

    chore: inserindo metodo de paginacao

--- src/main/java/br/com/fiap/global/baze/repository/DesafioRepository.java
+++ src/main/java/br/com/fiap/global/baze/repository/DesafioRepository.java
@@ -1,9 +1,12 @@
 package br.com.fiap.global.baze.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import br.com.fiap.global.baze.model.Desafio;
 
 public interface DesafioRepository extends JpaRepository<Desafio, Long> {
     
+    Page<Desafio> findByDescricaoContaining(String descricao, Pageable pageable);
 }
--- src/main/java/br/com/fiap/global/baze/repository/RegistroRepository.java
+++ src/main/java/br/com/fiap/global/baze/repository/RegistroRepository.java
@@ -1,9 +1,12 @@
 package br.com.fiap.global.baze.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.fiap.global.baze.model.Registro;
 
 public interface RegistroRepository extends JpaRepository<Registro, Long> {
     
+    Page<Registro> findByHoraRegistroContaining(String horaRegistro, Pageable pageable);
 }
